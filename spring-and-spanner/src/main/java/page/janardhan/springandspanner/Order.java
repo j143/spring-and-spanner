91b5bfa6ea4b62d98ca2b687807109338173cd28
Janardhan Pulivarthi <j143@protonmail.com>
2021-06-20 14:01:16 +0530

    add orders and orderitems classes

---
+++ spring-and-spanner/src/main/java/page/janardhan/springandspanner/Order.java
@@ -0,0 +1,23 @@
+package page.janardhan.labs;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.Data;
+import org.springframework.cloud.gcp.data.spanner.core.mapping.*;
+
+@Table(name="orders")
+@data
+public class Order {
+    @PrimaryKey
+    @Column(name="order_id")
+    private String id;
+
+    private String description;
+
+    @Column(name="creation_timestamp")
+    private LocalDateTime timestamp;
+
+    // to create one to many relationship with order items
+    @Interleaved
+    private List<OrderItem> items;
+}
---
+++ spring-and-spanner/src/main/java/page/janardhan/springandspanner/OrderItem.java
@@ -0,0 +1,23 @@
+package page.janardhan.labs;
+
+import lombok.Data;
+import org.springframework.cloud.gcp.data.spanner.core.mapping.*;
+
+
+@Table(name="order_items")
+@Data
+class OrderItem {
+    @PrimaryKey(keyOrder = 1)
+    @Column(name="order_id")
+    private String orderId;
+
+    // use composite primary keys
+    // order_id, and order_item_id
+
+    @PrimaryKey(keyOrder = 2)
+    @Column(name="order_item_id")
+    private String orderItemId;
+
+    private String description;
+    private Long quantity;
+}
