fb0549120b2e258d2c5cb228327fc0a7690049bf
Janardhan Pulivarthi <j143@protonmail.com>
2021-06-20 18:20:21 +0530

    add basic mapping /api/orders

--- spring-and-spanner/src/main/java/page/janardhan/springandspanner/OrderApp.java
+++ spring-and-spanner/src/main/java/page/janardhan/springandspanner/OrderApp.java
@@ -1,9 +1,12 @@
 package page.janardhan.springandspanner;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 
 @SpringBootApplication
@@ -19,6 +22,23 @@ class OrderController {
 
     OrderController(OrderRepository orderRepository) {
         this.orderRepository = orderRepository;
+    }
+
+    @GetMapping("/api/orders/{id}")
+    public Order getOrder(@PathVariable String id) {
+        return orderRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, id + " not found"));
+
+    }
+
+    @PostMapping("/api/orders")
+    public String createOrder(@RequestBody Order order) {
+        // Spanner does not auto Generate IDs
+        // Let's use UUID on new orders
+        order.setId(UUID.randomUUID().toString());
+        order.setTimestamp(LocalDateTime.now());
+        
+        return "1";
     }
 
     
