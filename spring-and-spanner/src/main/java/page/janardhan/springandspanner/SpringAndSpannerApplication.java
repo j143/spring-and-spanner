b534a57139b3b8f0dae8f600ae3a261460e7a495
Janardhan Pulivarthi <j143@protonmail.com>
2021-06-20 11:44:12 +0530

    fix BeanDefinitionOverrideException: Invalid bean definition with name 'gcpProjectIdProvider' defined in class path resource

---
+++ spring-and-spanner/src/main/java/page/janardhan/springandspanner/SpringAndSpannerApplication.java
@@ -0,0 +1,13 @@
+package page.janardhan.springandspanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndSpannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAndSpannerApplication.class, args);
+	}
+
+}
